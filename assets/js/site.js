0f6b54282bc7e296f11f345dc8bede3af49ec014
Trevhead0 <tloveday@hawk.iit.edu>
2019-04-20 22:58:42 -0500

    Add js for review checkout bin

--- assets/js/site.js
+++ assets/js/site.js
@@ -9,9 +9,6 @@
     // Make this top of everything!
     var sto = window.localStorage;
     var items=['Mozzarella Sticks', 'Spinach Dip', 'Shrimp Cocktail', 'Crab Ragoon', 'Lemon Chicken', 'Spicy Beef', 'Roasted Duck', 'Eggplant Parmesan', 'Italian Beef', 'Fettuccini Alfredo', 'Baked Mostaccioli', 'Spaghetti and Meatballs', 'Greek Salad', 'Ceaser Salad', 'House Salad', 'Baked Potato', 'Mashed Potato', 'Cannoli Bundle', 'Cheese Cake'];
-    console.log(items);
-    console.log(sto);
-
 
     // load previous local storage data
     if (storageAvailable('localStorage')){
@@ -19,17 +16,17 @@
         addPrevious();
         addEvents();
       }
+      if (document.querySelector('#checkoutpage') !==null){
+        document.querySelector('#review').classList.toggle('nv');
+        console.log("your in checkout now");
+        addReview();
+      }
     }
 
-    // delete previous form data but not all of local storage
-    document.querySelector("#mclear").addEventListener('click', function(){
-      var i;
-      for(i=0; i<19; i++){
-        sto.setItem(i, null);
-        console.log(sto.getItem(i));
-      }
-    });
-
+    // Get item discriptions
+    function getItems(i){
+      return items[i];
+    }
     // toggle function for focus css
     function focus(){
       if (document.querySelector('.focus')!==null){// this is to throw out error when no focus
@@ -54,6 +51,14 @@
       for(i=0; i<19; i++){
         foodnum='#'+'food'+(i+1);
         document.querySelector(foodnum).addEventListener('focus', focus);
+        // delete previous form data but not all of local storage
+        document.querySelector("#mclear").addEventListener('click', function(){
+          var i;
+          for(i=0; i<19; i++){
+            sto.setItem(i, null);
+            console.log(sto.getItem(i));
+          }
+        });
         document.querySelector(foodnum).addEventListener('change', function(e){
           var theid;
           if (e.target.id.length===5){
@@ -64,6 +69,91 @@
           }
           setquantity([theid]-1, e.target.value);
         });
+      }
+    }
+
+    // code for Adding review Section
+    function addReview(){
+      var i, li, pa, inum, ides, but1, but2, but3, input, itemnum;
+      for(i=0; i<19; i++){
+        itemnum =sto.getItem(i);
+        if(itemnum>0){
+          // declare the items elements
+          li= document.createElement('li');
+          pa= document.createElement('p');
+          inum= document.createElement('i');
+          ides= document.createElement('i');
+          but1= document.createElement('a');
+          but2= document.createElement('a');
+          but3= document.createElement('a');
+          input= document.createElement('input');
+          // Define item and quantity
+          li.classList.add('review-item');
+          li.id= i;
+          inum.id= 'inum'+i;
+          input.id='input'+i;
+          input.classList.add("food-number", "nv");
+          inum.innerText=sto.getItem(i);
+          ides.innerText=" x "+getItems(i);
+          pa.append(inum);
+          pa.append(input);
+          pa.append(ides);
+
+          // Define buttons
+          but1.id= 'but'+(i+1)+'-1';
+          but1.innerText = "Modify";
+          but1.href='#';
+          but1.classList.add('but');
+          but2.id= 'but'+(i+1)+'-2';
+          but2.href='#';
+          but2.innerText = "Delete";
+          but2.classList.add('but');
+          but3.id= 'but'+(i+1)+'-3';
+          but3.href='#';
+          but3.classList.add('nv');
+          but3.classList.add('but');
+          but3.innerText = "Confirm";
+          // but3.classList.add('nv');
+
+          // Add elements to list
+          li.append(pa);
+          li.append(but1);
+          li.append(but2);
+          li.append(but3);
+          document.querySelector('#review').append(li);
+
+
+          // add event listeners
+          document.querySelector('#'+but1.id).addEventListener('click', function(e){
+            // modify number====console.log(e.target.parentNode.firstChild.firstChild.innerText);
+            console.log(e.target.parentNode.firstChild);
+            e.target.parentNode.firstChild.firstChild.classList.toggle('nv');
+            e.target.parentNode.firstChild.childNodes[1].classList.toggle('nv');
+            e.target.parentNode.childNodes[1].classList.toggle('nv');
+            e.target.parentNode.childNodes[2].classList.toggle('nv');
+            e.target.parentNode.childNodes[3].classList.toggle('nv');
+          });
+          document.querySelector('#'+but2.id).addEventListener('click', function(e){
+            // delete entire parent
+            e.target.parentNode.remove();
+            setquantity(e.target.parentNode.id, null);
+            console.log(e.target.parentNode.id, null);
+
+          });
+          document.querySelector('#'+but3.id).addEventListener('click', function(e){
+            // delete entire parent
+            var value =document.querySelector('#input'+e.target.parentNode.id).value;
+            console.log(e.target.parentNode.id, value);
+            setquantity(e.target.parentNode.id, value);
+            e.target.parentNode.firstChild.firstChild.classList.toggle('nv');
+            e.target.parentNode.firstChild.childNodes[1].classList.toggle('nv');
+            e.target.parentNode.firstChild.firstChild.innerText=value;
+            e.target.parentNode.childNodes[1].classList.toggle('nv');
+            e.target.parentNode.childNodes[2].classList.toggle('nv');
+            e.target.parentNode.childNodes[3].classList.toggle('nv');
+          });
+
+        }
       }
     }
 
