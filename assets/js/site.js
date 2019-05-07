209bf105bf5045a73888e672edd93e3c6e5354ed
Trevhead0 <tloveday@hawk.iit.edu>
2019-05-06 19:14:04 -0500

    Make confirm page functions

--- assets/js/site.js
+++ assets/js/site.js
@@ -25,18 +25,48 @@
         document.querySelector('#review').classList.toggle('nv');
         console.log("your in checkout now");
         addReview();
+        document.querySelector('#checkbox').addEventListener('click', function(){
+          var today = new Date();
+          var dd = String(today.getDate()).padStart(2, '0');
+          var mm = String(today.getMonth() + 1).padStart(2, '0');
+          var yyyy = today.getFullYear();
+
+          today = yyyy+'-'+mm +'-'+ dd;
+          document.querySelector('#date').value=today;
+
+        });
+        document.querySelector('#checkouts').addEventListener('click', function (){
+          var del={};
+          var cc={};
+          var deltop, delinfo;
+          var deliveryinfo = document.getElementsByClassName('deliveryobj');
+          var ccinfo=document.getElementsByClassName('ccobj');
+
+          for (i=0; i<deliveryinfo.length; i++){
+            deltop = deliveryinfo[i].firstElementChild.innerText;
+            delinfo = deliveryinfo[i].lastElementChild.value;
+            del[deltop]=delinfo;
+          }
+          cc.Name=ccinfo[0].firstElementChild.innerText;
+          cc.Card=ccinfo[1].lastElementChild.value;
+          sto.setItem('deliveryobj', JSON.stringify(del));
+          sto.setItem('ccobj', JSON.stringify(cc));
+        });
       }
       if (document.querySelector('#confirmpage') !==null){
-        document.querySelector('#review').classList.toggle('nv');
         console.log("your in confirmation now");
-        addConfirm();
+        addOrder();
+        addDelivery();
+        addCard();
+        sto.clear();
       }
     }
 
     // code for adding previous data
     function addPrevious(){
       var item=sto.getItem('items');
-      if(item !==null){
+      // eslint doesnt like.  I like, grabs undefined as well.(i know i could write a second && not undefined.Same goes for line 102 and 181)
+      if(item !=null){
         item=JSON.parse(item);
         for(i=0; i<list.length; i++){
           list[i].firstElementChild.value=item[list[i].lastElementChild.innerText];
@@ -144,8 +174,86 @@
     }
 
     // code for confirm page
-    function addConfirm(){
+    function addOrder(){
+      var li, pa, inum, ides;
+      item=JSON.parse(sto.getItem('items'));
+      for (i=0; i<items.length; i++){
+        if (item[items[i]] != null && item[items[i]] !== ""){
+          // declare the items elements
+          li= document.createElement('li');
+          pa= document.createElement('p');
+          inum= document.createElement('i');
+          ides= document.createElement('i');
+          // Define item and quantity
+          li.classList.add('review-item');
+          li.id= items[i];
+          inum.innerText=item[items[i]];
+          ides.innerText=" x "+items[i];
+          pa.append(inum);
+          pa.append(ides);
 
+          // Add elements to list
+          li.append(pa);
+          document.querySelector('#review').append(li);
+        }
+      }
+    }
+
+    function addDelivery(){
+      var li, pa, inum, ides;
+      var del={};
+      var delnames;
+      del=JSON.parse(sto.getItem('deliveryobj'));
+      delnames = Object.getOwnPropertyNames(del);
+      for (i=0; i<delnames.length; i++){
+        // declare the items elements
+        li= document.createElement('li');
+        pa= document.createElement('p');
+        inum= document.createElement('i');
+        ides= document.createElement('i');
+        // Define item and quantity
+        li.classList.add('delivery-item');
+        li.id= delnames[i];
+        inum.innerText=del[delnames[i]];
+        ides.innerText=delnames[i]+" = ";
+        pa.append(ides);
+        pa.append(inum);
+        // Add elements to list
+        li.append(pa);
+        document.querySelector('#delivery').append(li);
+      }
+    }
+
+    function addCard(){
+      var li, pa, inum, ides, lastfour;
+      var cc={};
+      var ccnames;
+      cc=JSON.parse(sto.getItem('ccobj'));
+      ccnames = Object.getOwnPropertyNames(cc);
+
+      for (i=0; i<ccnames.length; i++){
+        // declare the items elements
+        li= document.createElement('li');
+        pa= document.createElement('p');
+        inum= document.createElement('i');
+        ides= document.createElement('i');
+        // Define item and quantity
+        li.classList.add('card-item');
+        li.id= ccnames[i];
+        if(i===1){
+          lastfour = cc[ccnames[i]].substring(12, 17);
+          inum.innerText="****-****-****-"+lastfour;
+        }
+        else{
+          inum.innerText=cc[ccnames[i]];
+        }
+        ides.innerText=ccnames[i]+" = ";
+        pa.append(ides);
+        pa.append(inum);
+        // Add elements to list
+        li.append(pa);
+        document.querySelector('#cc').append(li);
+      }
     }
 
   });
